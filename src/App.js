aa1da99e6adc62555ce396a2c3997e90a54556a9
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-06 18:00:01 +0400

    Modal Complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import Navbar from "./components/Navbar";
-import Modal from "./components/Modal";
 import CartContainer from "./components/CartContainer";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { calculateTotals } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
 function App() {
   const { cartItems } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
   return (
     <main>
-      <Modal />
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
