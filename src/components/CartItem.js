58e5ee132358e6780d0920d0a3d7a91ddbe56f52
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-06 15:52:56 +0400

    work with CartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,26 @@
-function CartItem() {
-  return <div>CartItem</div>;
+import { ChevronUp, ChevronDown } from "../icons";
+function CartItem({ id, img, title, price, amount }) {
+  return (
+    <article className="cart-item">
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">${price}</h4>
+        {/* remove button */}
+        <button className="remove-btn">remove</button>
+      </div>
+      <div>
+        {/* increase amount */}
+        <button className="amount-btn">
+          <ChevronUp />
+        </button>
+        <p className="amount">{amount}</p>
+        <button className="amount-btn">
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
+  );
 }
 
 export default CartItem;
